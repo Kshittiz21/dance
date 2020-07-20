01bcd105045821632f77c315e85cff5d36770be2
Kshittiz21 <59140501+Kshittiz21@users.noreply.github.com>
2020-07-20 16:28:21 +0530

    Add files via upload

---
+++ dance/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+from django.conf import settings
+app_name="dance"
+
+urlpatterns = [
+    # /dance/
+    path('', views.index, name='index'),
+]
